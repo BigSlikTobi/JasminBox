ce7ac5f40e9907e1aabdd168b66118daef32c9cd
BigSlikTobi <tobias.latta@hotmail.de>
2021-04-05 23:18:29 +0200

    4rdPush

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from "react";
+
+let defaultStyle = {
+  color: "black"
+}
+
+class Aggregate extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, display: "inline-block", padding: "10px"}}>
+            <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+class Filter extends Component {
+  render ()
+{
+  return(
+    <div>
+      <img/>
+      <input type="text" style={{margin:"10px"}}/>
+      Filter
+    </div>
+  );
+}}
+class Playlists extends Component {
+  render ()
+  {
+  return(
+    <div style={{...defaultStyle, width: "25%", display: "inline-block"}}>
+      <img/>
+      <h3>Playlist Name</h3>
+      <ul><li>Song1</li><li>Song2</li><li>Song3</li><li>Song4</li></ul>
+    </div>
+  )
+  }
+}
+
+function App() {
+  let name= "Tobi"
+  let color= "green"
+  return (
+    <div className="App">
+      <h1>Title</h1>
+      <Aggregate/>
+      <Aggregate/>
+      <Filter/>
+      <Playlists/>
+      <Playlists/>
+      <Playlists/>
+      <Playlists/>
+
+    </div>
+  );
+}
+
+export default App;
